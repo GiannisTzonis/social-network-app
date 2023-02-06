cbca77925f188de289ff7c8140bfd9a9822dcfa6
GiannisTzonis <tzonis.john@hotmail.com>
2023-02-06 14:59:17 +0200

    fixed profile page styles

--- routes/main.js
+++ routes/main.js
@@ -9,6 +9,8 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
+router.get("/feed", ensureAuth, postsController.likePost);
+router.put("/likePost/:id", postsController.likePost);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
--- routes/posts.js
+++ routes/posts.js
@@ -10,6 +10,7 @@ router.get("/:id", ensureAuth, postsController.getPost);
 router.post("/createPost", upload.single("file"), postsController.createPost);
 
 router.put("/likePost/:id", postsController.likePost);
+router.post("/likePost/:id", postsController.likePost);
 
 router.delete("/deletePost/:id", postsController.deletePost);
 
