9f38e1efacda4382a029d6e9c9ece7253c6a6187
GiannisTzonis <tzonis.john@hotmail.com>
2023-03-02 11:54:39 +0200

    fix error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,8 +46,8 @@ exports.postLogin = (req, res, next) => {
 
 exports.logout = (req, res) => {
   req.logout(() => {
-    console.log('User has logged out.')
-  })
+    console.log("User has logged out.");
+  });
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
@@ -69,9 +69,9 @@ exports.postSignup = (req, res, next) => {
   const validationErrors = [];
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." });
-  if (!validator.isLength(req.body.password, { min: 8 }))
+  if (!validator.isLength(req.body.password, { min: 4 }))
     validationErrors.push({
-      msg: "Password must be at least 8 characters long",
+      msg: "Passwords must be at least 4 characters long",
     });
   if (req.body.password !== req.body.confirmPassword)
     validationErrors.push({ msg: "Passwords do not match" });
